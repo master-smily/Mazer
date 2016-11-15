63a97758ce5aaf59b686301c8b8cf60934a455ac
master_smily <william.c.cedervall@gmail.com>
2016-11-15 14:46:39 +0100

    which way to go

--- environment/__init__.py
+++ environment/__init__.py
@@ -1,15 +1,13 @@
 import random
 
-from pygame import draw
-
-from main import GUI
+from pygame import draw, display
 
 
-def grid(gui, x, y, d=15, color=(255, 255, 255)):
+def grid(surface, x, y, d=15, color=(255, 255, 255)):
     """
 crates a grid
-    :param gui:
-    :type gui:
+    :param surface:
+    :type surface:
     :param x:
     :type x:
     :param y:
@@ -20,15 +18,17 @@ crates a grid
     :type color:
     """
     for i in range(x):
-        draw.line(gui, color, (i * d, 0), (i * d, y * d))
+        draw.line(surface, color, (i * d, 0), (i * d, y * d))
     for i in range(y):
-        draw.line(gui, color, (0, i * d), (x * d, i * d))
-    draw.rect(gui, (250, 0, 0), (0, 0, x * d + 1, y * d + 1), 1)
+        draw.line(surface, color, (0, i * d), (x * d, i * d))
+    draw.rect(surface, (250, 0, 0), (0, 0, x * d + 1, y * d + 1), 1)
+    display.update()
 
 
-def depth(x, y, d=15):
+def depth(surface, x, y, d=15):
     """
 creates maze with depth search algorithm.
+    :param surface:
     :param d:
     :type d:
     :param x:
@@ -36,37 +36,36 @@ creates maze with depth search algorithm.
     :param y:
     :type y:
     """
-    blocks = x * y
+    blocks = (x * y) - 1
     white = (255, 255, 255, 255)
     stack = []
     x_pos = random.randint(0, x - 1)
     y_pos = random.randint(0, y - 1)
     while blocks is not None:
         side = []
+        stack.append([x_pos, y_pos])
         # check if all sides are available
-        if GUI.get_at(x_pos + d / 2, y_pos) == white:
-            if [x_pos, y_pos - d] not in stack:
-                side.append('upp')
-        if GUI.get_at(x_pos + d, y_pos + 2 * d) == white:
-            if [x_pos + d, y_pos] not in stack:
-                side.append('right')
-        if GUI.get_at(x_pos + d / 2, y_pos - d) == white:
-            if [x_pos, y_pos + d] not in stack:
-                side.append('down')
-        if GUI.get_at(x_pos - d, y_pos + d * 2) == white:
-            if [x_pos - d, y_pos] not in stack:
-                side.append('left')
+        if surface.get_at(x_pos + d / 2, y_pos) == white and [x_pos, y_pos - d] not in stack:
+            side.append('upp')
+        if surface.gui.get_at(x_pos + d, y_pos + 2 * d) == white and [x_pos + d, y_pos] not in stack:
+            side.append('right')
+        if surface.gui.get_at(x_pos + d / 2, y_pos - d) == white and [x_pos, y_pos + d] not in stack:
+            side.append('down')
+        if surface.gui.get_at(x_pos - d, y_pos + d * 2) == white and [x_pos - d, y_pos] not in stack:
+            side.append('left')
+
         side.append('stop')
         side = side[random.randint(0, (side.index('stop') - 1))]
         if side == 'upp':
-            pass
-            # go upp
-        if side == 'right':
-            pass
-            # go right
-        if side == 'down':
-            pass
-            # go down
-        if side == 'left':
-            pass
-            # go left
+            y_pos -= d
+        elif side == 'right':
+            x_pos += d
+        elif side == 'down':
+            y_pos += d
+        elif side == 'left':
+            x_pos -= d
+        else: # go back one step
+            stack.pop()
+            x_pos = stack[-1][0]
+            y_pos = stack[-1][1]
+            stack.pop()
--- main.py
+++ main.py
@@ -12,8 +12,12 @@ from environment import grid
 x_blocks = 10  # int(input('x_blocks: '))
 y_blocks = 10  # int(input('y_blocks: '))
 length = 30
-GUI = display.set_mode([x_blocks * length + 1, y_blocks * length + 1])
+
+gui = display.set_mode([x_blocks * length + 1, y_blocks * length + 1])
 
 if __name__ == "__main__":
     pygame.init()
-    grid(GUI, x_blocks, y_blocks, length)
+    grid(gui, x_blocks, y_blocks, length)
+
+    while True:
+        display.update()
